b21b3256731a7729847ea27685f59604af50ecf5
SebTheN1 <sebastian.guerra@genstudents.org>
2025-05-29 09:47:25 -0400

    elimino apps.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Components/Home/Home'
 import Navbar from './Components/Navbar/Navbar'
-import Register from './Components/register/register'
+import Register from './Components/Register/register'
+
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home/>
+     
       <Register/>
     </>
   )
